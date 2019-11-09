559b07924c5721ce84d70d503db0c92bdc15045f
Shannon Lal <shannonlal@hotmail.com>
2019-11-09 10:49:46 -0500

    Check in simple jest config

--- src/components/room/Room.test.jsx
+++ src/components/room/Room.test.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Room from './Room';
 import { mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import Room from './Room';
 
 describe('Room Component Test', () => {
   beforeAll(() => {
@@ -16,8 +16,9 @@ describe('Room Component Test', () => {
   });
 
   it('should fail to render the query component because missing query function', () => {
-    const queryComponent = mount(<Room />);
+    const queryComponent = mount(<Room name="test" />);
 
+    console.log( 'Query Component', queryComponent);
     expect(queryComponent.find('p').length).toBe(1);
     expect(queryComponent.text()).toBe('Unexpected error creating Query field.  Missing property');
   });
