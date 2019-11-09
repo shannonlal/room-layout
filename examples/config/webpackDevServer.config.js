266ba68b919fc389b4817f05ac69089941da03bc
Shannon Lal <shannonlal@hotmail.com>
2019-11-09 13:39:27 -0500

    Clean up linting issues

--- examples/config/webpackDevServer.config.js
+++ examples/config/webpackDevServer.config.js
@@ -1,4 +1,3 @@
-'use strict';
 
 const errorOverlayMiddleware = require('react-dev-utils/errorOverlayMiddleware');
 const evalSourceMapMiddleware = require('react-dev-utils/evalSourceMapMiddleware');
--- src/components/room/Room.jsx
+++ src/components/room/Room.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import { useDrop } from 'react-dnd';
 import PropTypes from 'prop-types';
-import RoomItem from './RoomItem';
+import RoomItem, { ROOM_ITEM_TYPES } from './RoomItem';
 
-const Room = ({ name, id, items, styles, width, height, moveRoomItem, addRoomItem }) => (
-  <div sytle={{ ...styles, width, height }}>
-    {' '}
-    hi {name} id {id} items {items.length}{' '}
-  </div>
-);
+const Room = ({ name, id, items, styles, width, height, moveRoomItem, addRoomItem }) => {
+
+  const [, drop] = useDrop({
+    accept: [ROOM_ITEM_TYPES.ROOM_ITEM, ROOM_ITEM_TYPES.ADD_ROOM_ITEM],
+    drop(item, monitor) {
+      if (item.type === ROOM_ITEM_TYPES.ADD_ROOM_ITEM) {
+        const location = monitor.getClientOffset();
+        addRoomItem(id, location.x, location.y);
+      } else {
+        const delta = monitor.getDifferenceFromInitialOffset();
+        const left = Math.round(item.left + delta.x);
+        const top = Math.round(item.top + delta.y);
+        moveRoomItem(id, item.id, left, top);
+        return undefined;
+      }
+    },
+  });
+
+  return (
+    <div ref={drop} sytle={{ ...styles, width, height }}>
+      {' '}
+      hi {name} id {id} items {items.length}{' '}
+    </div>
+  );
+};
 
 Room.propTypes = {
   name: PropTypes.string.isRequired,
